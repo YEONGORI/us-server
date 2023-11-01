c1a787bb986fdf56e973fe0daf07c16b0ee60acc
YEONGORI <dusrjf14@gmail.com>
2023-11-01 20:09:53 +0900

    [init] 프로젝트 초기화

---
+++ src/main/java/us/usserver/UsServerApplication.java
@@ -0,0 +1,13 @@
+package us.usserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/us/usserver/UsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package us.usserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
