13e56f23e4e7c9e0f28d878ca40504d5e0976cee
YEONGORI <dusrjf14@gmail.com>
2023-11-05 17:22:29 +0900

    [feat] 회차(Chapter), 한줄(Paragraph) Entity 구현 완료

--- src/main/java/us/usserver/novel/Novel.java
+++ src/main/java/us/usserver/novel/Novel.java
@@ -1,10 +1,7 @@
 package us.usserver.novel;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,11 +10,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import us.usserver.base.BaseEntity;
+import us.usserver.chapter.Chapter;
 import us.usserver.novel.novelEnum.AgeRating;
 import us.usserver.novel.novelEnum.Genre;
 import us.usserver.novel.novelEnum.Hashtag;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -31,26 +31,33 @@ public class Novel extends BaseEntity {
     private Long id;
 
     @NotBlank
-    @Size(max = 30) // Length(max=30)으로 설정하면 한글은 10자 까지 입력 가능
+    @Size(max = 30) // Length(max=30)으로 설정 하면 한글은 10자 까지 입력 가능
     private String title;
 
     @NotBlank
+    @Lob
     private String thumbnail;
 
     @NotBlank
     @Size(max = 500)
-    private String author; // 종두님의 Author class로 교체 예정
+    private String author; // 종두 님의 Author class 로 교체 예정
 
     @NotBlank
     @Size(max = 500)
     private String synopsis;
 
     @NotBlank
+    @Enumerated(EnumType.STRING) // Enum 순서가 자주 변할 예정 이므로 String 으로 저장
     private Set<Hashtag> hashtag;
 
     @NotBlank
+    @Enumerated(EnumType.STRING)
     private Set<Genre> genre;
 
     @NotBlank
+    @Enumerated(EnumType.STRING)
     private Set<AgeRating> ageRating;
+
+    @OneToMany(mappedBy = "novel", cascade = CascadeType.ALL)
+    private List<Chapter> chapters = new ArrayList<>();
 }
---
+++ src/main/java/us/usserver/score/Score.java
@@ -0,0 +1,4 @@
+package us.usserver.score;
+
+public enum Score {
+}
