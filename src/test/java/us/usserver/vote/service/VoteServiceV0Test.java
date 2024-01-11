2fe6ed660815f06293cafcc36e7b94e3d7083d3c
YEONGORI <dusrjf14@gmail.com>
2024-01-11 16:29:05 +0900

    [test] 투표 기능 테스트 작성 완료

--- src/test/java/us/usserver/vote/service/VoteServiceV0Test.java
+++ src/test/java/us/usserver/vote/service/VoteServiceV0Test.java
@@ -1,5 +1,6 @@
 package us.usserver.vote.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,9 @@ import us.usserver.author.AuthorRepository;
 import us.usserver.chapter.Chapter;
 import us.usserver.chapter.ChapterMother;
 import us.usserver.chapter.ChapterRepository;
+import us.usserver.global.exception.AuthorNotAuthorizedException;
+import us.usserver.global.exception.DuplicatedVoteException;
+import us.usserver.global.exception.VoteNotFoundException;
 import us.usserver.member.Member;
 import us.usserver.member.MemberRepository;
 import us.usserver.member.memberEnum.Gender;
@@ -20,8 +24,12 @@ import us.usserver.novel.NovelRepository;
 import us.usserver.paragraph.Paragraph;
 import us.usserver.paragraph.ParagraphMother;
 import us.usserver.paragraph.ParagraphRepository;
+import us.usserver.vote.Vote;
 import us.usserver.vote.VoteRepository;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -45,7 +53,7 @@ class VoteServiceV0Test {
     private Author author;
     private Novel novel;
     private Chapter chapter;
-    private Paragraph paragraph1;
+    private Paragraph paragraph;
 
     @BeforeEach
     void setUp() {
@@ -53,15 +61,15 @@ class VoteServiceV0Test {
         setMember(author);
         novel = NovelMother.generateNovel(author);
         chapter = ChapterMother.generateChapter(novel);
-        paragraph1 = ParagraphMother.generateParagraph(author, chapter);
-        paragraph1.setSequenceForTest(0);
+        paragraph = ParagraphMother.generateParagraph(author, chapter);
+        paragraph.setSequenceForTest(0);
 
         novel.getChapters().add(chapter);
 
         authorRepository.save(author);
         novelRepository.save(novel);
         chapterRepository.save(chapter);
-        paragraphRepository.save(paragraph1);
+        paragraphRepository.save(paragraph);
     }
 
     @Test
@@ -70,23 +78,73 @@ class VoteServiceV0Test {
         // given
 
         // when
+        voteServiceV0.voting(paragraph.getId(), author.getId());
+        List<Vote> allByAuthor = voteRepository.findAllByAuthor(author);
 
         // then
+        assertThat(allByAuthor.size()).isEqualTo(1);
     }
 
     @Test
     @DisplayName("마음에 드는 한줄에 투표하기 - 중복 투표 방지")
-    void voting() {
+    void voting2() {
         // given
 
         // when
+        voteServiceV0.voting(paragraph.getId(), author.getId());
 
         // then
+        assertThrows(DuplicatedVoteException.class,
+                () -> voteServiceV0.voting(paragraph.getId(), author.getId()));
     }
 
     @Test
-    void unvoting() {
+    @DisplayName("투표 취소")
+    void unvoting1() {
+        // given
+        Vote vote = Vote.builder().author(author).paragraph(paragraph).build();
 
+        // when
+        voteRepository.save(vote);
+        voteServiceV0.unvoting(vote.getId(), author.getId());
+        List<Vote> allByAuthor = voteRepository.findAllByAuthor(author);
+
+        // then
+        assertThat(allByAuthor.size()).isZero();
+    }
+
+    @Test
+    @DisplayName("투표 취소 - 투표한 본인이 아닌 경우")
+    void unvoting2() {
+        // given
+        Author newAuthor = AuthorMother.generateAuthor();
+        setMember(newAuthor);
+
+        Vote vote = Vote.builder().author(author).paragraph(paragraph).build();
+
+        // when
+        authorRepository.save(newAuthor);
+        voteRepository.save(vote);
+
+        // then
+        assertThrows(AuthorNotAuthorizedException.class,
+                () -> voteServiceV0.unvoting(vote.getId(), newAuthor.getId()));
+    }
+
+    @Test
+    @DisplayName("투표 취소 - 중복해서 투표 취소가 요청 될 때")
+    void unvoting3() {
+        // given
+        Vote vote = Vote.builder().author(author).paragraph(paragraph).build();
+
+        // when
+        voteRepository.save(vote);
+        assertDoesNotThrow(
+                () -> voteServiceV0.unvoting(vote.getId(), author.getId()));
+
+        // then
+        assertThrows(VoteNotFoundException.class,
+                () -> voteServiceV0.unvoting(vote.getId(), author.getId()));
     }
 
     private void setMember(Author author) {
