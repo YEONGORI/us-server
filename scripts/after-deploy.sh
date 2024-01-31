444576a42170a408f8d37e1bc75782b1727f2d61
YEONGORI <dusrjf14@gmail.com>
2024-01-31 15:26:36 +0900

    [cicd] 배포 파일 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-BUILD_PATH=$(ls /home/api/server/build/*.jar)
+BUILD_PATH=$(ls /home/api/server/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_PATH)
-echo "> build 파일명: $JAR_NAME"
+echo "> build 파일명: $JAR_NAME" >> /home/api/log/deploy.log
 
-echo "> build 파일 복사"
+echo "> build 파일 복사" >> /home/api/log/deploy.log
 DEPLOY_PATH=/home/
 cp $BUILD_PATH $DEPLOY_PATH
 
-echo "> us-server-deploy.jar 교체"
+echo "> us-server-deploy.jar 교체" >> /home/api/log/deploy.log
 CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
 APPLICATION_JAR_NAME=us-server-deploy.jar
 APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
 
 ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
 
-echo "> 현재 실행중인 애플리케이션 pid 확인"
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/api/log/deploy.log
 CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
 
 if [ -z $CURRENT_PID ]
@@ -26,5 +26,5 @@ else
   sleep 5
 fi
 
-echo "> $APPLICATION_JAR 배포"
-nohup java -jar $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
+echo "> $APPLICATION_JAR 배포" >> /home/api/log/deploy.log
+nohup java -jar $APPLICATION_JAR >> /home/api/log/deploy.log 2> /home/api/log/deploy_err.log &
