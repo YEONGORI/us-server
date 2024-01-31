46ca05eb3e5f54830e2150a1d85d8ce539dc993b
YEONGORI <dusrjf14@gmail.com>
2024-01-31 15:39:11 +0900

    [cicd] build 파일 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-BUILD_PATH=$(ls /home/api/server/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_PATH)
-echo "> build 파일명: $JAR_NAME" >> /home/api/log/deploy.log
+BUILD_JAR=$(ls /home/api/server/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME"
 
-echo "> build 파일 복사" >> /home/api/log/deploy.log
-DEPLOY_PATH=/home/
-cp $BUILD_PATH $DEPLOY_PATH
+echo "> build 파일 복사"
+DEPLOY_PATH=/home/server/
+cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> us-server-deploy.jar 교체" >> /home/api/log/deploy.log
-CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
-APPLICATION_JAR_NAME=us-server-deploy.jar
-APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
+#echo "> us-server-deploy.jar 교체"
+#CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
+#APPLICATION_JAR_NAME=us-server-deploy.jar
+#APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
+#
+#ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
+#
+#echo "> 현재 실행중인 애플리케이션 pid 확인"
+#CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
 
-ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/api/log/deploy.log
-CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
+echo "> 현재 실행중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
@@ -26,5 +29,7 @@ else
   sleep 5
 fi
 
-echo "> $APPLICATION_JAR 배포" >> /home/api/log/deploy.log
-nohup java -jar $APPLICATION_JAR >> /home/api/log/deploy.log 2> /home/api/log/deploy_err.log &
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+
+echo "> DEPLOY_JAR 배포"
+nohup java -jar $DEPLOY_JAR
